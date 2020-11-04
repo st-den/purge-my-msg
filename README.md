6a36d06fdde63e9b076d2ee03607f1a44067b3f9
hematogender <hematogender@protonmail.com>
2020-11-04 23:18:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# purge-my-msg
+
+---
+> ⚠️ **Disclaimer**: make sure the name of the group is right, otherwise you might lose important information.
+---
+**purge-my-msg** is a Telegram user bot, which deletes all your messages from a chat you were kicked out of.
+
+Yes, you can "delete for everyone" even if you were kicked. No, there's no such option, at least not in official clients.
+
+## Usage
+
+```shell
+> purge-my-msg.py "Fancy Group Name"
+```
+
+And the output, hopefully:
+
+```sh
+1337 messages deleted.
+```
+
+## Requirements
+
+1. [Telethon](https://github.com/LonamiWebs/Telethon/) v1.17.6+. Currently in master, so:
+
+    ```shell
+    pip install https://github.com/LonamiWebs/Telethon/archive/master.zip
+    ```
+
+2. `api_id` and `api_hash` of your app, set up in `purge-my-msg.py`.
+
+    You can get those [here](https://my.telegram.org/apps).
---
+++ purge-my-msg.py
@@ -0,0 +1,39 @@
+from sys import argv
+from telethon.sync import TelegramClient
+from telethon.tl.types import PeerUser
+
+
+api_id = "Get yours here:"
+api_hash = "https://my.telegram.org/apps"
+
+client = TelegramClient("sesh", api_id, api_hash)
+
+
+async def main():
+    if len(argv) != 2:
+        print(f'Usage:\n    purge-my-msg.py "Fancy Group Name"')
+        exit(1)
+
+    chat = None
+
+    try:
+        chat = await client.get_input_entity(argv[1])
+    except:
+        await client.get_dialogs()  # populates cache
+        chat = argv[1]  # will resolve automatically anyways
+
+    me = PeerUser((await client.get_me(input_peer=True)).user_id)
+
+    messages = list()
+
+    async for message in client.iter_messages(chat):
+        if message.from_id == me:
+            messages.append(message)
+
+    await client.delete_messages(chat, messages)
+
+    print(len(messages), "messages deleted.")
+
+
+with client:
+    client.loop.run_until_complete(main())
